beefbf6754c47b5d3b108fb71b3b96b7a48d8d76
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-04-29 09:22:38 -0700

    commit5

---
+++ script.js
@@ -0,0 +1,62 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById('final-message-reveal-word');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+
+	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+	// Display wrong letters
+	wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
